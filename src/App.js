389f0e2e3de8988d21f592d467c17febf8006e5f
pindrive <kamilniewiadomski18@gmail.com>
2024-08-09 15:38:25 +0200

    refactor: move App.css file to another location

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-import './App.css';
+import './assets/App.css';
 
 function App() {
   return (
