8d3cf257cdb48af00f5e4721ca6946ed8c3656e0
pindrive <kamilniewiadomski18@gmail.com>
2024-08-09 14:30:56 +0200

    feat: add logo to NavigationBar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,9 @@
+import Logo from '../assets/Logo.jpg'
+
 const NavigationBar = () => {
   return (
     <nav>
+      <img src={Logo} alt="Little Lemon Logo"/>
         Navigation bar
     </nav>
 )};
