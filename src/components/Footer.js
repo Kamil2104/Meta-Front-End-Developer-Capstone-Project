10a178a396c455026261c3d4da9af13f654b9983
pindrive <kamilniewiadomski18@gmail.com>
2024-08-09 14:41:54 +0200

    feat: add navigation to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 const Footer = () => {
   return (
     <footer>
-        Footer navigation
+        <section className="doormatNavigation">
+          <h3> Doormat <br /> Navigation </h3>
+          <li> <a href='home.html'> Home </a> </li>
+          <li> <a href='about.html'> About </a> </li>
+          <li> <a href='menu.html'> Menu </a> </li>
+          <li> <a href='reservations.html'> Reservations </a> </li>
+          <li> <a href='order.html'> Order Online </a> </li>
+          <li> <a href='login.html'> Login </a> </li>
+        </section>
+        <section className="contact">
+          <h3> Contact </h3>
+          <li> <a href='address.html'> Address </a> </li>
+          <li> <a href='phone.html'> Phone number </a> </li>
+          <li> <a href='email.html'> Email </a> </li>
+        </section>
+        <section className="socialMediaLinks">
+          <h3> Social Media Links </h3>
+          <li> <a href='address.html'> Address </a> </li>
+          <li> <a href='phone.html'> Phone number </a> </li>
+          <li> <a href='email.html'> Email </a> </li>
+        </section>
     </footer>
 )};
 
